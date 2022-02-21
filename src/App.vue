79b1d5c91a5feef782c9b2df22bb8199adec59b6
Jose Vega <jocvegar@gmail.com>
2022-02-20 20:11:13 -0600

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,87 @@
-<script setup></script>
-
 <template>
-  <div
-    class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
-  >
-    <div class="flex-1 flex justify-between sm:hidden">
-      <a
-        href="#"
-        class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-      >
-        Previous
-      </a>
-      <a
-        href="#"
-        class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-      >
-        Next
-      </a>
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <div>
+      <word-row
+        v-for="(guess, i) in state.guesses"
+        :key="i"
+        :value="guess"
+        :solution="state.solution"
+        :submitted="i < state.currentGuessIndex"
+      />
     </div>
-    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-      <div>
-        <p class="text-sm text-gray-700">
-          Showing
-          {{ " " }}
-          <span class="font-medium">1</span>
-          {{ " " }}
-          to
-          {{ " " }}
-          <span class="font-medium">10</span>
-          {{ " " }}
-          of
-          {{ " " }}
-          <span class="font-medium">97</span>
-          {{ " " }}
-          results
-        </p>
-      </div>
-      <div>
-        <nav
-          class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-          aria-label="Pagination"
-        >
-          <a
-            href="#"
-            class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-          >
-            <span class="sr-only">Previous</span>
-            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-          </a>
-          <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-          <a
-            href="#"
-            aria-current="page"
-            class="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-          >
-            1
-          </a>
-          <a
-            href="#"
-            class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-          >
-            2
-          </a>
-          <a
-            href="#"
-            class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
-          >
-            3
-          </a>
-          <span
-            class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"
-          >
-            ...
-          </span>
-          <a
-            href="#"
-            class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
-          >
-            8
-          </a>
-          <a
-            href="#"
-            class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-          >
-            9
-          </a>
-          <a
-            href="#"
-            class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-          >
-            10
-          </a>
-          <a
-            href="#"
-            class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-          >
-            <span class="sr-only">Next</span>
-            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-          </a>
-        </nav>
-      </div>
-    </div>
+    <p v-if="wonGame" class="text-base text-center">
+      🏆 Congrats you solved it!
+    </p>
+    <p v-else-if="lostGame" class="text-base text-center">😔 Out of tries.</p>
+    <simple-keyboard
+      @onKeyPress="handleOnKeyPress"
+      :guessedLetters="state.guessedLetters"
+    />
   </div>
 </template>
 
-<style></style>
+<script setup>
+import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import WordRow from "./components/WordRow.vue";
+import { reactive, onMounted, onBeforeUnmount, computed } from "vue";
+
+const state = reactive({
+  solution: "books",
+  guesses: ["", "", "", "", "", ""],
+  currentGuessIndex: 0,
+  guessedLetters: {
+    miss: [],
+    found: [],
+    hint: [],
+  },
+});
+
+const wonGame = computed(
+  () => state.guesses[state.currentGuessIndex - 1] === state.solution
+);
+
+const lostGame = computed(() => !wonGame.value && state.currentGuessIndex >= 6);
+
+const handleOnKeyPress = (key) => {
+  if (state.currentGuessIndex >= 6 || wonGame.value) return;
+
+  const currentGuess = state.guesses[state.currentGuessIndex];
+
+  if (key == "{enter}") {
+    if (currentGuess.length == 5) {
+      state.currentGuessIndex++;
+      setTimeout(() => {
+        for (var i = 0; i < currentGuess.length; i++) {
+          let c = currentGuess.charAt(i);
+          if (c == state.solution.charAt(i)) {
+            state.guessedLetters.found.push(c);
+          } else if (state.solution.indexOf(c) != -1) {
+            state.guessedLetters.hint.push(c);
+          } else {
+            state.guessedLetters.miss.push(c);
+          }
+        }
+      }, 2000);
+    }
+  } else if (key == "{bksp}") {
+    state.guesses[state.currentGuessIndex] = currentGuess.slice(0, -1);
+  } else if (currentGuess.length < 5) {
+    const alphaRegex = /[a-zA-Z]/;
+    if (alphaRegex.test(key)) state.guesses[state.currentGuessIndex] += key;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let k =
+      e.key == "Enter" ? "{enter}" : e.key == "Backspace" ? "{bksp}" : e.key;
+    handleOnKeyPress(k);
+  });
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("keyup", handleOnKeyPress(), false);
+});
+</script>
